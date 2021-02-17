1eb048b419f944e5d892e63b8acebdaa3b365310
U117331 <U117331@mobi.ch>
2021-02-17 11:47:37 +0100

    merge mit vote

--- src/app/poker-lobby/poker-lobby.component.ts
+++ src/app/poker-lobby/poker-lobby.component.ts
@@ -1,5 +1,4 @@
 import {Component, EventEmitter, Output} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-poker-lobby',
@@ -13,10 +12,6 @@ export class PokerLobbyComponent {
 
   name;
   room;
-  testVar;
-
-  constructor(private http: HttpClient) {
-  }
 
   emitName(): void {
     this.nameEmitter.emit(this.name);
@@ -24,11 +19,5 @@ export class PokerLobbyComponent {
 
   emitRoom(): void {
     this.roomEmitter.emit(this.room);
-  }
-
-  executeTest(): void {
-    console.log(`${this.name} joined ${this.room}.`);
-    // this.testVar = this.http.get('https://planningpoker-server.azurewebsites.net/rooms/ATM/votes');
-    this.testVar = this.http.post(`https://planningpoker-server.azurewebsites.net/rooms/${this.room}/vote`, {name: this.name, vote: '3'});
   }
 }
--- src/app/poker-table/poker-table.component.ts
+++ src/app/poker-table/poker-table.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 
+import {HttpClient} from '@angular/common/http';
+
 @Component({
   selector: 'app-poker-table',
   templateUrl: './poker-table.component.html',
@@ -7,13 +9,23 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class PokerTableComponent implements OnInit {
 
-  constructor() {
+  constructor(private http: HttpClient) {
   }
 
   @Input() name;
   @Input() room;
 
+  testVar;
+
   ngOnInit(): void {
   }
 
+  vote(vote): void {
+    console.log(`${this.name} voted ${vote} in ${this.room}.`);
+    // this.testVar = this.http.get('https://planningpoker-server.azurewebsites.net/rooms/ATM/votes');
+    this.testVar = this.http.post(`https://planningpoker-server.azurewebsites.net/rooms/${this.room}/vote`, {
+      name: this.name,
+      vote
+    });
+  }
 }
