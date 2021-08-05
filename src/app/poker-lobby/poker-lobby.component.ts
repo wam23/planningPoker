8db54bdb1473c754856b5553764aa5f62231e0c3
Rafael Wampfler (U114379) <rafael.wampfler@mobi.ch>
2021-08-05 14:24:14 +0200

    dynamic cards

--- src/app/poker-lobby/poker-lobby.component.ts
+++ src/app/poker-lobby/poker-lobby.component.ts
@@ -1,9 +1,10 @@
-import {Component, EventEmitter, Output} from '@angular/core';
-import {NgForm} from '@angular/forms';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 export interface User {
   name: string;
   room: string;
+  cards: string;
 }
 
 @Component({
@@ -15,8 +16,25 @@ export class PokerLobbyComponent {
 
   @Output() userEmitter: EventEmitter<User> = new EventEmitter();
 
-  save(form: NgForm): void {
-    const user = {name: form.value.name, room: form.value.room} as User;
-    this.userEmitter.emit(user);
+  cardsets = {
+    "Simple Fibonacci": "1,2,3,5,8,13,20,?",
+    "Fibonacci ": "0,1,2,3,5,8,13,21,34,55,89,?",
+    "Mountain Goat": "0,0.5,1,2,3,5,8,13,20,40,100,?",
+    "Sequential": "0,1,2,3,4,5,6,7,8,9,10,?",
+    "T-Shirt": "XS,S,M,L,XL,?"
+  }
+
+  userForm = new FormGroup({
+    room: new FormControl(''),
+    name: new FormControl(''),
+    cards: new FormControl(this.cardsets['Simple Fibonacci'])
+  });
+
+  save(): void {
+    this.userEmitter.emit(this.userForm.value);
+  }
+
+  setCards(newCards: string) {
+    this.userForm.get('cards').setValue(newCards);
   }
 }
