e28fff5546380cc351694ce8d1beb4335a79a259
Rafael Wampfler (U114379) <U114379@mobi.ch>
2022-05-12 15:15:15 +0200

    backend mocks

--- cypress/integration/startpage.spec.ts
+++ cypress/integration/startpage.spec.ts
@@ -1,4 +1,12 @@
 describe('planningPoker', () => {
+
+  beforeEach(() => {
+    cy.intercept("/cardset", { fixture: "cardset.json" });
+    cy.intercept("/poll/*/init", { statusCode: 200 });
+    cy.intercept("/poll/*", { fixture: "poll.json", delayMs: 5000 });
+    cy.intercept("/rooms/*/vote", { statusCode: 204 });
+  });
+
   it('load start page', () => {
     cy.visit('/')
     cy.get('.jumbotron').contains('Planning Poker')
