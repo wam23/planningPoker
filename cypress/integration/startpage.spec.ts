aea28b13fdf1e95adcf9747af5c54b557f5c7aa4
Rafael Wampfler (U114379) <U114379@mobi.ch>
2022-05-12 16:10:42 +0200

    login and vote test

--- cypress/integration/startpage.spec.ts
+++ cypress/integration/startpage.spec.ts
@@ -1,14 +1,31 @@
 describe('planningPoker', () => {
 
   beforeEach(() => {
-    cy.intercept("/cardset", { fixture: "cardset.json" });
-    cy.intercept("/poll/*/init", { statusCode: 200 });
-    cy.intercept("/poll/*", { fixture: "poll.json", delayMs: 5000 });
-    cy.intercept("/rooms/*/vote", { statusCode: 204 });
+    cy.intercept("/cardset", { fixture: "cardset.json" }).as('get cardset');
+    cy.intercept("/poll/*/init", { statusCode: 200 }).as('init poll');
+    cy.intercept("/poll/*", { fixture: "poll.json", delayMs: 2000 }). as('long polling');
+    cy.intercept("/rooms/*/vote", { statusCode: 204 }).as('send vote');
   });
 
   it('load start page', () => {
-    cy.visit('/')
-    cy.get('.jumbotron').contains('Planning Poker')
-  })
+    cy.visit('/');
+    cy.get('.jumbotron').contains('Planning Poker');
+  });
+
+  it('login to room', () => {
+    cy.get('input[name="room"]').type('Scrum');
+    cy.get('input[name="name"]').type('John');
+    cy.get('.btn').click();
+    cy.get('.lead').contains('John in Scrum');
+    
+    cy.wait(2000).as('wait for long-poll update');
+    cy.get('.result-name').contains('test');
+    cy.get('.result-mini-card').contains('5');
+  });
+
+  it('vote in room', () => {
+    cy.get('.container > :nth-child(4)').click();
+    cy.get('.card-selected').contains('3');
+    cy.get('small').contains('gespeichert');
+  });
 })
